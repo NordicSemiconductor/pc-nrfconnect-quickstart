8c6d7895161dbbc41efe8efad028e13c6b9db81a
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-07-27 13:57:23 +0200

    refactor: use new shared button variant instead of custom button

--- src/components/Learn.tsx
+++ src/components/Learn.tsx
@@ -28,16 +28,15 @@ export default ({ back, next }: { back: () => void; next: () => void }) => {
                 </Heading>
                 <div className="tw-flex tw-flex-col tw-items-center tw-gap-4 tw-pt-10">
                     {deviceLinks(device).map(({ label, href }) => (
-                        // I cannot use shared Buttons here because of the &:focus:not[disabled] styles on the secondary variant.
-                        // Tailwind classes seem to not be able to overwrite it (they have less weight) and have not decided on whether this should be another variant yet.
-                        <button
-                            type="button"
+                        <Button
                             key={label}
+                            variant="link-button"
                             onClick={() => openUrl(href)}
-                            className="tw-h-8 tw-w-96 tw-border tw-border-solid tw-border-primary tw-px-4 tw-text-left tw-text-sm tw-text-primary active:tw-bg-gray-50"
+                            large
+                            className="tw-w-96 tw-text-left"
                         >
                             {label}
-                        </button>
+                        </Button>
                     ))}
                 </div>
             </Main.Content>
