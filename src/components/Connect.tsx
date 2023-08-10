a8857d596bc41b3b31fd811a1f468fefb169cd5b
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-08-10 14:36:44 +0200

    feat: for the user test we will select the first device we get after a min timeout

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -4,14 +4,10 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Device } from '@nordicsemiconductor/nrf-device-lib-js';
 
-import {
-    DeviceIcon,
-    deviceName,
-    isSupportedDevice,
-} from '../features/deviceGuides';
+import { isSupportedDevice } from '../features/deviceGuides';
 import {
     connectedDevicesEvents,
     getConnectedDevices,
@@ -19,18 +15,22 @@ import {
 import Heading from './Heading';
 import Main from './Main';
 
-const invokeIfSpaceOrEnterPressed =
-    (onClick: React.KeyboardEventHandler<Element>) =>
-    (event: React.KeyboardEvent) => {
-        event.stopPropagation();
-        if (event.key === ' ' || event.key === 'Enter') {
-            onClick(event);
-        }
-    };
-
 export default ({ next }: { next: (device: Device) => void }) => {
     const [connectedDevices, setConnectedDevices] = useState<Device[]>([]);
     const [longSearchDuration, setLongSearchDuration] = useState(false);
+    const [hasWaitedMinDuration, setHasWaitedMinDuration] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setHasWaitedMinDuration(true);
+        }, 3000);
+    }, []);
+
+    useEffect(() => {
+        if (hasWaitedMinDuration && connectedDevices.length) {
+            next(connectedDevices[0]);
+        }
+    }, [hasWaitedMinDuration, connectedDevices, next]);
 
     useEffect(() => {
         setConnectedDevices(getConnectedDevices());
@@ -70,39 +70,16 @@ export default ({ next }: { next: (device: Device) => void }) => {
                         ? 'Select your kit'
                         : 'Connect a Nordic kit to your PC'}
                 </Heading>
-                <div className="tw-p-8">
-                    <div className="scrollbar tw-max-h-40 tw-w-48 tw-bg-gray-50">
-                        {connectedDevices.map(device => (
-                            <div
-                                key={device.serialNumber}
-                                tabIndex={0}
-                                role="button"
-                                onKeyUp={invokeIfSpaceOrEnterPressed(() =>
-                                    next(device)
-                                )}
-                                onClick={() => next(device)}
-                                className="tw-flex tw-w-full tw-cursor-pointer tw-flex-row tw-items-center tw-gap-1 tw-border-b tw-border-solid tw-px-2 tw-py-1 last:tw-border-b-0 hover:tw-bg-white"
-                            >
-                                <DeviceIcon
-                                    device={device}
-                                    className="tw-h-5 tw-w-6 tw-fill-gray-700"
-                                />
-                                <div className="tw-flex tw-flex-col tw-text-left">
-                                    <p>
-                                        <b>{deviceName(device)}</b>
-                                    </p>
-                                    <p>{device.serialNumber}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
                 <div className="tw-flex tw-flex-col tw-justify-center tw-gap-4">
                     <p>Searching icon/spinner</p>
                     <p>Searching for devices</p>
                     {longSearchDuration && (
                         <div>
                             <p>This is taking a while.</p>
+                            <p>
+                                Make sure one of the supported devices is
+                                connected: nRF9160DK
+                            </p>
                             <p>
                                 {' '}
                                 If this persists, contact support on{' '}
