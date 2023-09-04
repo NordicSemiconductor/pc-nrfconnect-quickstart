a57116ee0f65ce41f9f4b0ec7606e9e7b31437eb
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-09-04 13:58:57 +0200

    feat: Add Link component with different colors

---
+++ src/common/Link.tsx
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+import React from 'react';
+import { classNames } from '@nordicsemiconductor/pc-nrfconnect-shared';
+
+export default ({
+    label,
+    href,
+    color = 'tw-text-gray-700',
+}: {
+    label: string;
+    href: string;
+    color?: string;
+}) => (
+    <a
+        target="_blank"
+        rel="noreferrer noopener"
+        href={href}
+        onClick={event => event.stopPropagation()}
+        className={classNames('tw-underline', color, `hover:${color}`)}
+    >
+        {label}
+    </a>
+);
