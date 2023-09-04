2199602cfd1360de28ca302349a21d2aa899b2f0
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-09-04 13:59:34 +0200

    feat: Change develop to new design

--- src/features/steps/Develop.tsx
+++ src/features/steps/Develop.tsx
@@ -4,52 +4,96 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React from 'react';
-import {
-    openWindow,
-    usageData,
-} from '@nordicsemiconductor/pc-nrfconnect-shared';
+import React, { useState } from 'react';
+import { usageData } from '@nordicsemiconductor/pc-nrfconnect-shared';
 
+// @ts-expect-error svg imports work
+import vscodeIcon from '../../../resources/vscode.svg';
+// @ts-expect-error svg imports work
+import vscodeAltIcon from '../../../resources/vscode-alt.svg';
 import { Back } from '../../common/Back';
+import Italic from '../../common/Italic';
+import ListSelect, {
+    DisabledListItem,
+    SelectableListItem,
+} from '../../common/ListSelect';
 import Main from '../../common/Main';
 import { Next } from '../../common/Next';
 
-export default () => (
-    <Main>
-        <Main.Content
-            heading="Install nRF Connect SDK and Toolchain"
-            className="tw-max-w-sm"
-        >
-            <div className="tw-flex tw-flex-col tw-items-center tw-gap-4 tw-pt-10">
-                <p>
-                    The nRF Connect SDK is where you begin building low-power
-                    wireless applications with Nordic Semiconductor.
-                </p>
-                <p>
-                    The Toolchain Manager app is used to install and manage IDE,
-                    SDK, Toolchain and dependencies.
-                </p>
-                <p>
-                    Launch the app and install the latest version. Follow the
-                    instructions in the Toolchain Manager app to complete the
-                    setup of Ide, SDK and Toolchain.
-                </p>
-            </div>
-        </Main.Content>
-        <Main.Footer>
-            <Back />
-            <Next
-                label="Launch Toolchain Manager"
-                onClick={next => {
-                    usageData.sendUsageData('Launch Toolchain Manager');
-                    openWindow.openApp({
-                        name: 'pc-nrfconnect-toolchain-manager',
-                        source: 'official',
-                    });
+export default () => {
+    const [selected, setSelected] = useState<
+        SelectableListItem | DisabledListItem
+    >();
 
-                    next();
-                }}
-            />
-        </Main.Footer>
-    </Main>
-);
+    const items = [
+        {
+            id: 'vscode',
+            selected: selected?.id === 'vscode',
+            content: (
+                <div className="tw-flex tw-flex-row tw-items-start tw-justify-start tw-text-sm">
+                    <div className="tw-w-32 tw-flex-shrink-0 tw-pr-10">
+                        <div className="tw-flex tw-flex-col tw-items-center tw-gap-4">
+                            <b>VS Code IDE</b>
+                            <img
+                                src={
+                                    selected?.id === 'vscode'
+                                        ? vscodeAltIcon
+                                        : vscodeIcon
+                                }
+                                alt="VS Code icon"
+                                className="tw-h-10 tw-w-10"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        The <Italic>nRF Connect for Visual Studio Code</Italic>{' '}
+                        extension is the recommended development environment for
+                        building and debugging applications based on the{' '}
+                        <Italic>nRF Conenct SDK</Italic>.
+                    </div>
+                </div>
+            ),
+        },
+        {
+            id: 'cli',
+            selected: selected?.id === 'cli',
+            content: (
+                <div className="tw-flex tw-flex-row tw-items-start tw-justify-start tw-text-sm">
+                    <div className="tw-w-32 tw-flex-shrink-0 tw-pr-5">
+                        <div>
+                            <b>Command Line</b>
+                        </div>
+                    </div>
+                    <div>
+                        The <Italic>nRF Util</Italic> and <Italic>west</Italic>{' '}
+                        command line tools can be used to manually configure
+                        your custom environment
+                    </div>
+                </div>
+            ),
+        },
+    ];
+
+    return (
+        <Main>
+            <Main.Content heading="How would you like to start developing">
+                <ListSelect items={items} onSelect={setSelected} />
+            </Main.Content>
+            <Main.Footer>
+                <Back />
+                <Next label="Skip" />
+                <Next
+                    label="Continue"
+                    disabled={!selected}
+                    onClick={next => {
+                        usageData.sendUsageData(
+                            `Selected developing option: ${selected?.id}`
+                        );
+
+                        next();
+                    }}
+                />
+            </Main.Footer>
+        </Main>
+    );
+};
