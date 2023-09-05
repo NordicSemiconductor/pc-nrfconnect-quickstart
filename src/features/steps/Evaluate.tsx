b8de45aea0d616e9c8e08e93930390a1e45f2313
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-09-05 11:05:21 +0200

    feat: update design in evaluate step and add back/next buttons. also send usagedata

--- src/features/steps/Evaluate.tsx
+++ src/features/steps/Evaluate.tsx
@@ -9,10 +9,14 @@ import {
     apps,
     Button,
     DownloadableApp,
+    ExternalLink,
+    usageData,
 } from '@nordicsemiconductor/pc-nrfconnect-shared';
 
 import { useAppSelector } from '../../app/store';
+import { Back } from '../../common/Back';
 import Main from '../../common/Main';
+import { Next } from '../../common/Next';
 import { getChoiceUnsafely } from '../device/deviceSlice';
 
 export default () => {
@@ -32,33 +36,51 @@ export default () => {
     return (
         <Main>
             <Main.Content heading={`Evaluate ${choice.name}`}>
-                {choice.evaluationResources.map(
-                    ({ links, app, description }) => (
-                        <div
-                            key={app}
-                            className="tw-flex tw-flex-row tw-justify-between"
-                        >
-                            <div className="tw-flex tw-flex-col">
-                                <div>
-                                    <b>{getAppName(app)}</b>
-                                </div>
-                                {description}
-                                <br />
-                                {links.map(({ label, href }) => (
-                                    <div key={label} />
-                                ))}
-                            </div>
-                            <Button
-                                variant="link-button"
-                                size="xl"
-                                onClick={() => {}}
+                <div className="tw-flex tw-flex-col tw-gap-6">
+                    {choice.evaluationResources.map(
+                        ({ links, app, description }) => (
+                            <div
+                                key={app}
+                                className="tw-flex tw-flex-row tw-justify-between"
                             >
-                                Open {getAppName(app)}
-                            </Button>
-                        </div>
-                    )
-                )}
+                                <div className="tw-w-80">
+                                    <div>
+                                        <b>{getAppName(app)}</b>
+                                    </div>
+                                    {description}
+                                    {links.map(({ label, href }) => (
+                                        <div
+                                            key={label}
+                                            className="tw-pt-0.5 tw-text-xs"
+                                        >
+                                            <ExternalLink
+                                                label={label}
+                                                href={href}
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
+                                <Button
+                                    variant="link-button"
+                                    size="xl"
+                                    onClick={() => {
+                                        usageData.sendUsageData(
+                                            `Opened app ${app}`
+                                        );
+                                        // install and open or something
+                                    }}
+                                >
+                                    Open {getAppName(app)}
+                                </Button>
+                            </div>
+                        )
+                    )}
+                </div>
             </Main.Content>
+            <Main.Footer>
+                <Back />
+                <Next />
+            </Main.Footer>
         </Main>
     );
 };
