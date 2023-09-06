17c2e7ee8de7ecba8dfde5ddfed3d1cae0a40066
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-09-06 15:37:28 +0200

    feat: new present step design

--- src/features/steps/Present.tsx
+++ src/features/steps/Present.tsx
@@ -4,46 +4,71 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 import { useAppDispatch, useAppSelector } from '../../app/store';
 import { Back } from '../../common/Back';
+import Link from '../../common/Link';
 import Main from '../../common/Main';
 import { Next } from '../../common/Next';
-import {
-    deviceDescription,
-    DeviceIcon,
-    deviceName,
-} from '../device/deviceGuides';
+import { deviceDescription } from '../device/deviceGuides';
 import { getSelectedDeviceUnsafely, selectDevice } from '../device/deviceSlice';
+
+const overWriteA = ({
+    href,
+    children,
+}: {
+    href?: string;
+    children?: React.ReactNode;
+}) => <Link color="tw-text-primary" label={children} href={href || ''} />;
 
 export default () => {
     const device = useAppSelector(getSelectedDeviceUnsafely);
     const dispatch = useAppDispatch();
+    const [currentSubStepIndex, setCurrentSubStepIndex] = useState(
+            ? 0
+    );
 
     return (
         <Main>
             <Main.Content
-                heading={`Let's get started with the ${deviceName(device)}`}
+                heading={deviceDescription(device)[currentSubStepIndex].title}
             >
-                <div className="tw-pb-10">
-                    <DeviceIcon
-                        device={device}
-                        className="tw-h-14 tw-w-20 tw-fill-gray-700"
-                    />
-                </div>
-                <div className="tw-max-w-sm tw-pt-10">
-                    <p>{deviceDescription(device)}</p>
-                </div>
+                <ReactMarkdown
+                    components={{
+                        a: overWriteA,
+                    }}
+                >
+                    {
+                        deviceDescription(device)[currentSubStepIndex]
+                            .markdownContent
+                    }
+                </ReactMarkdown>
             </Main.Content>
             <Main.Footer>
                 <Back
                     onClick={back => {
-                        dispatch(selectDevice(undefined));
-                        back();
+                        if (currentSubStepIndex > 0) {
+                            setCurrentSubStepIndex(currentSubStepIndex - 1);
+                        } else {
+                            dispatch(selectDevice(undefined));
+                            back();
+                        }
                     }}
                 />
-                <Next />
+                <Next
+                    onClick={next => {
+                        if (
+                            currentSubStepIndex <
+                            deviceDescription(device).length - 1
+                        ) {
+                            setCurrentSubStepIndex(currentSubStepIndex + 1);
+                        } else {
+                            next();
+                        }
+                    }}
+                />
             </Main.Footer>
         </Main>
     );
