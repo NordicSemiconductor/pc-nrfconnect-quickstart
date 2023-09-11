f82952031188b9d8f665396eee1fd08783fb7d50
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-09-11 17:22:16 +0200

    Fix naming
    - Unifify the naming of “VS Code” to always use a capitalised “C”
    - That thing on the left side is called the “Activity Bar”, according to
      https://code.visualstudio.com/docs/getstarted/userinterface

--- src/features/steps/develop/VsCodeOpened.tsx
+++ src/features/steps/develop/VsCodeOpened.tsx
@@ -6,7 +6,7 @@
 
 import React from 'react';
 
-import sidebarImage from '../../../../resources/vs_code_sidebar.png';
+import activityBarImage from '../../../../resources/vs_code_activity_bar.png';
 import { useAppDispatch } from '../../../app/store';
 import { Back } from '../../../common/Back';
 import Main from '../../../common/Main';
@@ -24,8 +24,8 @@ export default () => {
             >
                 <div className="tw-flex">
                     <img
-                        src={sidebarImage}
-                        alt="VS code sidebar with the nRF Connect icon higlighted"
+                        src={activityBarImage}
+                        alt="VS Code activity bar with the nRF Connect icon higlighted"
                     />
                     <div className="tw-relative">
                         <span className="mdi mdi-arrow-left-thick tw-absolute tw-top-[127px] tw-text-3xl" />
