a54894d18d4cf94d81e336c9b5d0335b785ab485
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-10-05 09:23:38 +0200

    feat: create common resource and resource withButton component

--- src/features/steps/Learn.tsx
+++ src/features/steps/Learn.tsx
@@ -8,9 +8,9 @@ import React from 'react';
 
 import { useAppSelector } from '../../app/store';
 import { Back } from '../../common/Back';
-import Link from '../../common/Link';
 import Main from '../../common/Main';
 import { Next } from '../../common/Next';
+import { Resource } from '../../common/Resource';
 import { getLearnStep } from '../device/deviceGuides';
 import { getSelectedDeviceUnsafely } from '../device/deviceSlice';
 
@@ -21,22 +21,9 @@ export default () => {
         <Main>
             <Main.Content heading="Recommended learning resources">
                 <div className="tw-flex tw-flex-col tw-items-start tw-justify-start tw-gap-6">
-                    {getLearnStep(device).resources.map(
-                        ({ label, description, link }) => (
-                            <div key={label}>
-                                <b>{label}</b>
-                                <br />
-                                {description}
-                                <div className="tw-pt-0.5 tw-text-xs">
-                                    <Link
-                                        label={link.label}
-                                        href={link.href}
-                                        color="tw-text-primary"
-                                    />
-                                </div>
-                            </div>
-                        )
-                    )}
+                    {getLearnStep(device).resources.map(props => (
+                        <Resource {...props} key={props.label} />
+                    ))}
                 </div>
             </Main.Content>
             <Main.Footer>
