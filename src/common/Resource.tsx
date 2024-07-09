8b20a1758239055c12ef449f61e823a149685331
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2024-07-09 09:07:29 +0200

    refactor: make appResourceButton a component and offload some logic to it

--- src/common/Resource.tsx
+++ src/common/Resource.tsx
@@ -4,9 +4,16 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React from 'react';
-import { Button } from '@nordicsemiconductor/pc-nrfconnect-shared';
+import React, { useEffect, useState } from 'react';
+import {
+    apps,
+    Button,
+    openWindow,
+    telemetry,
+} from '@nordicsemiconductor/pc-nrfconnect-shared';
 
+import { useAppSelector } from '../app/store';
+import { getSelectedDeviceUnsafely } from '../features/device/deviceSlice';
 import Link from './Link';
 
 interface Link {
@@ -33,7 +40,7 @@ export const Resource = ({ label, description, link }: ResourceProps) => (
 
 interface ResourceWithButtonProps {
     title: string;
-    description: string;
+    description: string | React.ReactNode;
     buttonLabel: string;
     onClick: () => void;
     links?: Link[];
@@ -71,3 +78,91 @@ export const ResourceWithButton = ({
         </Button>
     </div>
 );
+
+export const AppResourceButton = ({
+    description,
+    links,
+    disabled,
+    app,
+    onInstallStart,
+    onInstallFinish,
+}: {
+    description: string;
+    app: string;
+    links?: Link[];
+    disabled?: boolean;
+    onInstallStart?: () => void;
+    onInstallFinish?: () => void;
+}) => {
+    const device = useAppSelector(getSelectedDeviceUnsafely);
+    const [displayName, setDisplayName] = useState(app);
+    const [isInstalling, setIsInstalling] = useState(false);
+
+    useEffect(() => {
+        apps.getDownloadableApps().then(({ apps: receivedApps }) => {
+            const updatedAppInfo = receivedApps.find(a => a.name === app);
+            setDisplayName(updatedAppInfo?.displayName || app);
+        });
+    }, [app]);
+
+    return (
+        <div className="tw-flex tw-flex-row tw-justify-between tw-gap-10">
+            <div className="tw-w-80">
+                <div>
+                    <b>{displayName}</b>
+                </div>
+                {description}
+                {links?.map(({ label, href }) => (
+                    <div key={label} className="tw-pt-0.5 tw-text-xs">
+                        <Link
+                            label={label}
+                            href={href}
+                            color="tw-text-primary"
+                        />
+                    </div>
+                ))}
+            </div>
+            <Button
+                variant="link-button"
+                size="xl"
+                disabled={disabled || isInstalling}
+                onClick={async () => {
+                    const appInfo = await apps
+                        .getDownloadableApps()
+                        .then(({ apps: receivedApps }) =>
+                            receivedApps.find(
+                                a => a.name === app && a.source === 'official'
+                            )
+                        );
+
+                    if (appInfo && !apps.isInstalled(appInfo)) {
+                        setIsInstalling(true);
+                        onInstallStart?.();
+                        await apps.installDownloadableApp(appInfo);
+                        setIsInstalling(false);
+                        onInstallFinish?.();
+                    }
+
+                    openWindow.openApp(
+                        {
+                            name: app,
+                            source: 'official',
+                        },
+                        {
+                            device: {
+                                serialNumber: device.serialNumber,
+                            },
+                        }
+                    );
+
+                    telemetry.sendEvent('Opened evaluation app', {
+                        app,
+                    });
+                }}
+                className="tw-flex-1"
+            >
+                {isInstalling ? 'Installing...' : `Open ${displayName}`}
+            </Button>
+        </div>
+    );
+};
