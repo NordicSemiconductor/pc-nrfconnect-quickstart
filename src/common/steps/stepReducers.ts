62b59c697bcaae4108870073a92a761c1449b723
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2024-07-09 10:33:29 +0200

    fix: reset choice when selecting device

--- src/common/steps/stepReducers.ts
+++ src/common/steps/stepReducers.ts
@@ -7,6 +7,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import type { AppThunk } from '../../app/store';
+import { setChoice } from '../../features/device/deviceSlice';
 import { setFinishedLastStep } from '../../features/flow/flowSlice';
 import develop, { reset as developReset } from './develop/developSlice';
 import program, { reset as programReset } from './program/programSlice';
@@ -15,6 +16,7 @@ import verification, {
 } from './verify/verificationSlice';
 
 export const allReset = (): AppThunk => dispatch => {
+    dispatch(setChoice(undefined));
     dispatch(programReset());
     dispatch(verificationReset());
     dispatch(developReset());
