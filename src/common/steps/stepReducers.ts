647ff3b7f8250edb6cbc0952e45b81289b835add
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2024-08-07 13:25:16 +0200

    feat: add verify steps and samples

--- src/common/steps/stepReducers.ts
+++ src/common/steps/stepReducers.ts
@@ -9,8 +9,11 @@ import { combineReducers } from '@reduxjs/toolkit';
 import type { AppThunk } from '../../app/store';
 import { setChoice } from '../../features/device/deviceSlice';
 import { setFinishedLastStep } from '../../features/flow/flowSlice';
+import verification5x, {
+    reset as verification5xReset,
+} from './5xFamilyVerify/verifySlice';
 import verification91, {
-    reset as verificationReset,
+    reset as verification91Reset,
 } from './91FamilyVerify/verificationSlice';
 import develop, { reset as developReset } from './develop/developSlice';
 import program, { reset as programReset } from './program/programSlice';
@@ -18,9 +21,15 @@ import program, { reset as programReset } from './program/programSlice';
 export const allReset = (): AppThunk => dispatch => {
     dispatch(setChoice(undefined));
     dispatch(programReset());
-    dispatch(verificationReset());
     dispatch(developReset());
+    dispatch(verification91Reset());
+    dispatch(verification5xReset());
     dispatch(setFinishedLastStep(false));
 };
 
-export default combineReducers({ verification91, develop, program });
+export default combineReducers({
+    verification5x,
+    verification91,
+    develop,
+    program,
+});
