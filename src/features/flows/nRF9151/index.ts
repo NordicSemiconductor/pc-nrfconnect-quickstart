b037306b4571ec4786265728ccc28c7e29451e35
Mia Pickel <Mia.Pickel@nordicsemi.no>
2025-09-16 12:19:07 +0200

    fix: asset tracker template uses shell mode

--- src/features/flows/nRF9151/index.ts
+++ src/features/flows/nRF9151/index.ts
@@ -159,7 +159,7 @@ const verificationConfig = {
         {
             ref: 'Asset Tracker',
             vComIndex: 0,
-            mode: 'LINE' as const,
+            mode: 'SHELL' as const,
         },
         {
             ref: 'AT Commands',
