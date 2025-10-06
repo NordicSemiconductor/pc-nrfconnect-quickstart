c6a14260f0b10e556e89af8a8b692fb47d9fb1f7
Mia Pickel <Mia.Pickel@nordicsemi.no>
2025-10-06 13:28:35 +0200

    feat: implement new programming architecture
    Can now switch more easily between different ways of programming
    Added action-list programming type

---
+++ src/common/steps/program/actionVariants/actionList.ts
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+import { NrfutilDeviceLib } from '@nordicsemiconductor/pc-nrfconnect-shared/nrfutil/device';
+import path from 'path';
+
+import { AppThunk, RootState } from '../../../../app/store';
+import { getFirmwareFolder } from '../../../../features/device/deviceGuides';
+import { DeviceWithSerialnumber } from '../../../../features/device/deviceLib';
+import { ActionListEntry } from '../../../../features/device/deviceSlice';
+import type { ProgrammingConfig } from '../programEffects';
+import { setError, setProgrammingProgress } from '../programSlice';
+
+export default (
+        actionList: ActionListEntry[]
+    ): AppThunk<RootState, ProgrammingConfig> =>
+    dispatch => {
+        const array: ProgrammingConfig['actions'] = [];
+
+        const addActionEntry = (item: ProgrammingConfig['actions'][number]) =>
+            array.push(item);
+
+        const actions = actionList.map(action => {
+            switch (action.type) {
+                case 'programming': {
+                    const { file, core, link } = action.firmware;
+                    const coreLabel = core
+                        ? `${core} core`
+                        : 'Connectivity bridge';
+
+                    const index =
+                        addActionEntry({
+                            title: coreLabel,
+                            link,
+                        }) - 1;
+                    return async (device: DeviceWithSerialnumber) => {
+                        try {
+                            await NrfutilDeviceLib.program(
+                                device,
+                                path.join(getFirmwareFolder(), file),
+                                ({ totalProgressPercentage: progress }) =>
+                                    dispatch(
+                                        setProgrammingProgress({
+                                            index,
+                                            progress,
+                                        })
+                                    ),
+                                core,
+                                undefined
+                            );
+                        } catch (e) {
+                            dispatch(
+                                setError({
+                                    icon: 'mdi-flash-alert-outline',
+                                    text: `Failed to program the ${coreLabel}`,
+                                })
+                            );
+                            throw e;
+                        }
+                    };
+                }
+                case 'wait':
+                    return () =>
+                        new Promise(resolve => {
+                            setTimeout(resolve, action.durationMs);
+                        });
+                default:
+                    return () => {};
+            }
+        });
+
+        return {
+            run: device =>
+                actions.reduce(
+                    (acc, next) =>
+                        acc.then(async () => {
+                            await next(device);
+                        }),
+                    Promise.resolve()
+                ),
+            actions: array,
+        };
+    };
